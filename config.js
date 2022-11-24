e5d5c341e8e199798025221022f33601a01421b4
Rashneema <79697800+Rashneema@users.noreply.github.com>
2022-11-24 08:47:43 +0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+//replace it with student's configuration
+//make sure to write EXPORT keyword
+export const firebaseConfig = {
+  apiKey: "AIzaSyBRIHl2oioclZhFuFZk7vLto0epesi1ZSo",
+  authDomain: "storytelling-41b9d.firebaseapp.com",
+  projectId: "storytelling-41b9d",
+  storageBucket: "storytelling-41b9d.appspot.com",
+  messagingSenderId: "812066787960",
+  appId: "1:812066787960:web:72fedb64c840c51246d38d"
+};
+
+
